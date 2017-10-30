f69c5139992dd393cf01a1448c20a0e873efb1bd
Levi VonBank <levi.vonbank@mnsu.edu>
2017-10-30 16:37:46 +0000

    Google Oauth made through omniauth2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  class Application < Rails::Application
+    config.web_console.whiny_requests = false
+  end
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -38,4 +41,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150809022253) do
+ActiveRecord::Schema.define(version: 20171030161139) do
 
   create_table "items", force: :cascade do |t|
     t.string   "category"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20150809022253) do
     t.datetime "post_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
 end
