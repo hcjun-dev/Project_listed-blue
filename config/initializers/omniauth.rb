cd6a8f622103018852d91a15cce8bd2780732e7e
Levi VonBank <levi.vonbank@mnsu.edu>
2017-11-01 19:48:32 +0000

    omniauth changed into 2 in omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
-OmniAuth.config.logger = Rails.logger
+OmniAuth2.config.logger = Rails.logger
 
-Rails.application.config.middleware.use OmniAuth::Builder do
+Rails.application.config.middleware.use OmniAuth2::Builder do
   provider :google_oauth2, '276398849280-tldubmh2lrp3vcsj3oaofjvv5ga8ttg8.apps.googleusercontent.com', 'vhYzMBVBBk6EkJu1uPT-BEht', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
