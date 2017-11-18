57db7a2c4017e54c336e29727fa140c689783dfd
Levi VonBank <levi.vonbank@mnsu.edu>
2017-11-18 21:36:17 +0000

    Bootstrap added for the comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
     
-        def create 
+    def create 
         @item = Item.find(params[:item_id])
         @comment = @item.comments.create(params[:comment].permit(:name, :body))
         redirect_to item_path(@item)
